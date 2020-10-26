715a86165bddc35c20fa6fb3c3a983d745aa3d94
Your Name <you@example.com>
2020-10-26 11:52:10 -0400

    requirements

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+conda create -n disease_prog python=3.7.3
+conda activate disease_prog
+conda install pytorch cudatoolkit torchvision matplotlib scipy numpy pandas h5py ipython jupyterlab tensorflow-gpu keras cython seaborn scikit-learn r
+conda install -c conda-forge tslearn ipdb
+pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines pytorch-lightning
+pip install git+https://github.com/rtqichen/torchdiffeq
+conda install -c conda-forge ncurses
+pip install pytorch-lightning
+pip install pytest 
